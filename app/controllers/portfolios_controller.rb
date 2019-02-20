6f8a3c38cb03b6c00b7c8836e32a57fe7c0ba51d
chris <chris.gresock@gmail.com>
2019-02-20 18:55:18 -0500

    Added portfolio edit and update methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,4 +18,19 @@ class PortfoliosController < ApplicationController
     def new
         @portfolio_item = Portfolio.new
     end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        respond_to do |format|
+            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle,:body))
+              format.html { redirect_to portfolios_path, notice: 'Portfolio entry was updated successfully!' }
+            else
+              format.html { render :edit }
+            end
+          end
+    end
 end
