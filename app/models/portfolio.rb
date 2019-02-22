235adb7a39abbffba1e2ee3f29da5e7e151d8f87
chris <chris.gresock@gmail.com>
2019-02-22 14:47:43 -0500

    added defaults for portfolios feature

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,4 +10,12 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'Ruby on Rails')
     end
 
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://placehold.it/600x400"
+        self.thumb_image ||= "http://placehold.it/350x200"
+
+    end
+
 end
