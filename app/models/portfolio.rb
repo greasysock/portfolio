43885c104a2d4fee8ae5d9b261c59548bbf82338
chris <chris.gresock@gmail.com>
2019-02-22 12:34:33 -0500

    Added seeds for Topics and updated Blogs seeds. Added scopes for portfolios.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,12 @@ class Portfolio < ApplicationRecord
 
     validates_presence_of :title, :body, :main_image, :thumb_image
 
+    def self.angular
+        where(subtitle: 'Angular')
+    end
+
+    def self.ruby
+        where(subtitle: 'Ruby on Rails')
+    end
+
 end
