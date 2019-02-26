3ce01be1c7070f5c66faf884424bfaa8df4393c6
chris <chris.gresock@gmail.com>
2019-02-26 15:13:53 -0500

    Added copyright and modified theme.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
     include SessionParameters
     include CurrentUserConcern
     include DefaultPageContent
+
+    before_action :set_copyright
+    def set_copyright
+        @copyright = GresockViewTool::Renderer.copyright "Chris Gresock", "All rights reserved"
+    end
+end
+
+module GresockViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   before_action :set_page_title_action
-  layout "blogs"
 
   # GET /blogs
   # GET /blogs.json
