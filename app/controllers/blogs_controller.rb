d551288bb8244de0ce4ecb704d51cce6915e03b4
chris <chris.gresock@gmail.com>
2019-02-27 11:02:59 -0500

    Implemented authorization for portfolio and blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   before_action :set_page_title_action
+  access all: [:show, :index], site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :destroy, :show]
+  access all: [:show, :index], site_admin: :all
     def index
         @portfolio_items = Portfolio.all
     end
