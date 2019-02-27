f817fece8796e85d34106eb28ac878a0914c0891
chris <chris.gresock@gmail.com>
2019-02-27 17:34:17 -0500

    created blog layout

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,8 @@
 module BlogsHelper
+    def admin_controls blog
+        if logged_in?(:site_admin)
+            content_tag(:a, "Edit", class: "btn btn-outline-warning", href: edit_blog_path(blog)) +
+            content_tag(:a, "Delete", class: "btn btn-outline-danger", href: url_for(blog), method: :delete, data: { confirm: 'Are you sure?' })
+        end
+    end
 end
