7387d8d6ce08444ef6f2450e2288399b898fbf7e
chris <chris.gresock@gmail.com>
2019-09-05 21:05:34 -0400

    added onClick event to redirect directly to live pdf

--- app/javascript/components/PdfView.jsx
+++ app/javascript/components/PdfView.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { Document, Page } from 'react-pdf/dist/entry.webpack'
 
 export default class PdfView extends React.Component {
+
     render() {
+        const pdfLink = `/assets/resume.pdf?version=${Date.now()}`
+
         return (
-            <Document file={`/assets/resume.pdf?version=${Date.now()}`}>
+            <Document onClick={()=>{window.location = pdfLink}} file={pdfLink}>
                 <Page pageNumber={1} scale={2} />
             </Document>
         )
